42f2fc52ddffd6e75c56e6a3051597f16934fb13
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-26 15:42:59 -0500

    feat(auth): add store to control the actions

---
+++ src/modules/stores/auth.store.ts
@@ -0,0 +1,27 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { defineStore } from 'pinia'
+import { computed } from 'vue'
+import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+
+export const useAuthStore = defineStore('auth', () => {
+  const auth = useFirebaseAuth()
+
+  if (!auth) {
+    return null
+  }
+
+  const user = useCurrentUser()
+  const login = (email: string, passwrd: string) => signInWithEmailAndPassword(auth, email, passwrd)
+  const logout = () => signOut(auth)
+  const resetPassword = (mail: string) => sendPasswordResetEmail(auth, mail)
+
+  const isAuthenticated = computed(() => !!user.value)
+
+  return {
+    user,
+    isAuthenticated,
+    login,
+    logout,
+    resetPassword,
+  }
+})
