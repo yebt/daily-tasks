9e2a2a366edef149ad9032746ef2e6064d441c84
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-26 10:10:48 -0500

    feat(vite): add new alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const aliases: Record<string, string> = {
+  '@': './src',
+  '@core': './src/core',
+  '@shared': './src/shared',
+  '@modules': './src/modules',
+}
+
+const alias = Object.entries(aliases).reduce((acc: Record<string, string>, [aKey, aVal]) => {
+  acc[aKey] = fileURLToPath(new URL(aVal, import.meta.url))
+  return acc
+}, {})
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    UnoCSS(),
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [UnoCSS(), vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias,
+    // alias: {
+    //   '@': fileURLToPath(new URL('./src', import.meta.url)),
+    // },
   },
 })
