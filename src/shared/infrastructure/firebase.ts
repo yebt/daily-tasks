369843a496b4b459832dc2dd3664b5f1219b0e9a
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2026-01-26 15:45:32 -0500

    feat(shared/infra): change firebase config position

--- src/shared/infrastructure/firebase.ts
+++ src/shared/infrastructure/firebase.ts
@@ -2,15 +2,17 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
-export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyD8A3tVl73PSxMgRilHh2MXqDdoSfWnqoE",
-  authDomain: "vue-wrkshps.firebaseapp.com",
-  projectId: "vue-wrkshps",
-  storageBucket: "vue-wrkshps.firebasestorage.app",
-  messagingSenderId: "832372852884",
-  appId: "1:832372852884:web:8d95c791abe4c979740c81",
-  measurementId: "G-ZTG5Y2NL8R"
-})
+const firebaseConfig = {
+  apiKey: 'AIzaSyD8A3tVl73PSxMgRilHh2MXqDdoSfWnqoE',
+  authDomain: 'vue-wrkshps.firebaseapp.com',
+  projectId: 'vue-wrkshps',
+  storageBucket: 'vue-wrkshps.firebasestorage.app',
+  messagingSenderId: '832372852884',
+  appId: '1:832372852884:web:8d95c791abe4c979740c81',
+  measurementId: 'G-ZTG5Y2NL8R',
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
 
 export const firebaseDb = getFirestore(firebaseApp)
 export const firebaseAuth = getAuth(firebaseApp)
